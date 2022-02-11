37f5a75c943672190999e464237d931a9c590ab4
Kevin Phan <90020140+KevinFPhan@users.noreply.github.com>
2022-02-11 00:47:37 -0800

    testing

---
+++ app.js
@@ -0,0 +1,33 @@
+function updateList() {
+	const titles = [...document.querySelectorAll('.sect')].sort((a, b) => {
+		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+	});
+
+	document.querySelectorAll(".selected-sect").forEach(c => c.classList.remove("selected-sect"));
+	
+	document.querySelectorAll(".nav-hl")[[...document.querySelectorAll('.sect')].indexOf(titles[0])].classList.add("selected-sect");
+}
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+})
+
+function myFunction() {
+    document.querySelector("#myDropdown").classList.toggle("show");
+    console.log("hello");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+  
+        var dropdowns = document.getElementsByClassName("dropdown-content1");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
